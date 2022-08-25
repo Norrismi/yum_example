36dd0b2465eeb4c038a10fd0e2a6dd698a70ed16
Norrismi <Norrismi0@gmail.com>
2022-08-25 11:09:03 -0400

    first

---
+++ schemas/menu.js
@@ -0,0 +1,11 @@
+export default{
+
+        title: 'Menu',
+        name: 'menu',
+        type: 'document',
+        fields: [
+            { name: 'foodItem', type: 'string', title: 'Food Item' },
+            { name: 'price', type: 'string', title: 'Price' },
+        ]
+    }
+
